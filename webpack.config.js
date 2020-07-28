0a7f649f112eb59ef44fe684ebec66698ddfb0f6
Tatiana Kudashova <kudashova93@gmail.com>
2020-07-28 17:20:07 +0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", "json"],
+    alias: {
+      components: path.resolve(__dirname, "src/components/"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
+  },
+  devtool: "source-map",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "dist"),
+    port: 3040,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CaseSensitivePathsPlugin(),
+  ],
+};
