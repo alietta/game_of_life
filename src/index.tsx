b3b19ee91ba1b2a0f781ce5e573e76ed7e338200
Tatiana Kudashova <kudashova93@gmail.com>
2020-08-04 21:34:31 +0400

    make app

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import { App } from "@/modules/App";
 import "./index.css";
 
-render(<div>This it my game</div>, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
---
+++ src/modules/App/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <div>This is my future game</div>
+    </div>
+  );
+};
