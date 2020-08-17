8ed806b28e2e58a189cfb0d1b70d65182a97a631
Tatiana Kudashova <kudashova93@gmail.com>
2020-08-17 21:59:39 +0400

    resize cubes

--- src/modules/Field/index.tsx
+++ src/modules/Field/index.tsx
@@ -6,18 +6,36 @@ import { FieldWrapper } from './FieldStyles';
 export const Field: FunctionComponent = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const viewRef = useRef<obelisk.PixelView | null>(null);
   const { fieldSize, cellSize } = useStoreSelector(state => state.settings);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
-      const width = fieldSize * cellSize;
+      const width = 900;
       canvas.width = width * 2;
       canvas.height = width * 2;
       canvas.style.width = `${width}px`;
       canvas.style.height = `${width}px`;
       if (ctxRef && ctxRef.current) {
         ctxRef.current.scale(2, 2);
+      }
+      if (viewRef.current) {
+        viewRef.current.clear();
+        const dimension = new obelisk.CubeDimension(
+          cellSize,
+          cellSize,
+          cellSize
+        );
+        const gray = obelisk.ColorPattern.GRASS_GREEN;
+        const color = new obelisk.CubeColor().getByHorizontalColor(gray);
+
+        const cube = new obelisk.Cube(dimension, color, true);
+        const p = new obelisk.Point3D(0, 0, 0);
+        viewRef.current.renderObject(cube, p);
+        const post = fieldSize * cellSize - cellSize;
+        const p2 = new obelisk.Point3D(post, post, 0);
+        viewRef.current.renderObject(cube, p2);
       }
     }
   }, [fieldSize, cellSize]);
@@ -27,18 +45,9 @@ export const Field: FunctionComponent = () => {
     if (canvas) {
       const context = canvas.getContext('2d');
       ctxRef.current = context;
-      const point = new obelisk.Point(fieldSize * cellSize, cellSize);
+      const point = new obelisk.Point(450, 50);
       const pixelView = new obelisk.PixelView(canvas, point);
-      const dimension = new obelisk.CubeDimension(cellSize, cellSize, cellSize);
-      const gray = obelisk.ColorPattern.GRASS_GREEN;
-      const color = new obelisk.CubeColor().getByHorizontalColor(gray);
-
-      const cube = new obelisk.Cube(dimension, color, true);
-      const p = new obelisk.Point3D(0, 0, 0);
-      pixelView.renderObject(cube, p);
-      const post = fieldSize * cellSize - cellSize;
-      const p2 = new obelisk.Point3D(post, post, 0);
-      pixelView.renderObject(cube, p2);
+      viewRef.current = pixelView;
     }
   }, []);
 
