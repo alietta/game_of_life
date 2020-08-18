84c648c1608c679b4c033dddb8218e159f8b4ef3
Tatiana Kudashova <kudashova93@gmail.com>
2020-08-18 22:34:53 +0400

    game play

--- src/modules/Field/helper.ts
+++ src/modules/Field/helper.ts
@@ -56,3 +56,34 @@ export const makeField = (
   });
   return filledArray;
 };
+
+export const checkNeighbor = (field: number[][], x: number, y: number) => {
+  const rules = [
+    [x - 1, y + 1],
+    [x, y + 1],
+    [x + 1, y + 1],
+    [x - 1, y],
+    [x + 1, y],
+    [x - 1, y - 1],
+    [x, y - 1],
+    [x + 1, y - 1],
+  ];
+  const width = field[0].length;
+  const height = field.length;
+  const neighbor = rules.filter(([xRule, yRule]: [number, number]) => {
+    const ruleInField =
+      yRule >= 0 && xRule >= 0 && xRule < width && yRule < height;
+    return ruleInField && field[yRule][xRule] > 0;
+  });
+  return neighbor.length;
+};
+
+export const getGeneration = (field: number[][]): number[][] => {
+  return field.map((row: number[], y: number) => {
+    return row.map((life: number, x: number) => {
+      const neighbor = checkNeighbor(field, x, y);
+      const alive = life > 0 ? neighbor < 4 && neighbor > 1 : neighbor === 3;
+      return alive ? life + 1 : 0;
+    });
+  });
+};
--- src/modules/Field/index.tsx
+++ src/modules/Field/index.tsx
@@ -4,7 +4,7 @@ import { useStoreSelector } from '@/hooks/useStoreSelector';
 import obelisk from 'obelisk.js';
 import { actions } from './duck/reducer';
 import { FieldWrapper } from './FieldStyles';
-import { makeField } from './helper';
+import { makeField, randomByPercent, getGeneration } from './helper';
 
 interface Props {
   presentation?: boolean;
@@ -15,31 +15,11 @@ export const Field: FunctionComponent<Props> = props => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
   const viewRef = useRef<obelisk.PixelView | null>(null);
-  const { fieldSize, cellSize } = useStoreSelector(state => state.settings);
+  const { fieldSize, cellSize, percent } = useStoreSelector(
+    state => state.settings
+  );
+  const { current: speed } = useStoreSelector(state => state.speed);
   const { cells, active } = useStoreSelector(state => state.field);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      /* if (viewRef.current) { */
-      /*   viewRef.current.clear(); */
-      /*   const dimension = new obelisk.CubeDimension( */
-      /*     cellSize, */
-      /*     cellSize, */
-      /*     cellSize */
-      /*   ); */
-      /*   const gray = obelisk.ColorPattern.GRASS_GREEN; */
-      /*   const color = new obelisk.CubeColor().getByHorizontalColor(gray); */
-
-      /*   const cube = new obelisk.Cube(dimension, color, true); */
-      /*   const p = new obelisk.Point3D(0, 0, 0); */
-      /*   viewRef.current.renderObject(cube, p); */
-      /*   const post = fieldSize * cellSize - cellSize; */
-      /*   const p2 = new obelisk.Point3D(post, post, 0); */
-      /*   viewRef.current.renderObject(cube, p2); */
-      /* } */
-    }
-  }, [fieldSize, cellSize]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -56,16 +36,16 @@ export const Field: FunctionComponent<Props> = props => {
       const pixelView = new obelisk.PixelView(canvas, point);
       viewRef.current = pixelView;
     }
-    if (presentation) {
-      dispatch(
-        actions.setActive([
-          0,
-          fieldSize - 1,
-          fieldSize * fieldSize - 1,
-          fieldSize * (fieldSize - 1),
-        ])
-      );
-    }
+    /* if (presentation) { */
+    /*   dispatch( */
+    /*     actions.setActive([ */
+    /*       0, */
+    /*       fieldSize - 1, */
+    /*       fieldSize * fieldSize - 1, */
+    /*       fieldSize * (fieldSize - 1), */
+    /*     ]) */
+    /*   ); */
+    /* } */
   }, []);
 
   useEffect(() => {
@@ -80,30 +60,47 @@ export const Field: FunctionComponent<Props> = props => {
           if (item > 0) {
             const cube = new obelisk.Cube(dimension, color, true);
             const p = new obelisk.Point3D(cellSize * x, cellSize * y, 0);
-            viewRef.current.renderObject(cube, p);
+            viewRef!.current!.renderObject(cube, p);
           }
         });
       });
     }
   }, [cells, cellSize]);
 
+  /* useEffect(() => { */
+  /*   if (presentation) { */
+  /*     dispatch( */
+  /*       actions.setActive([ */
+  /*         0, */
+  /*         fieldSize - 1, */
+  /*         fieldSize * fieldSize - 1, */
+  /*         fieldSize * (fieldSize - 1), */
+  /*       ]) */
+  /*     ); */
+  /*   } */
+  /* }, [fieldSize, cellSize]); */
+
   useEffect(() => {
-    if (presentation) {
-      dispatch(
-        actions.setActive([
-          0,
-          fieldSize - 1,
-          fieldSize * fieldSize - 1,
-          fieldSize * (fieldSize - 1),
-        ])
-      );
-    }
-  }, [fieldSize]);
+    dispatch(actions.setActive(randomByPercent(fieldSize, fieldSize, percent)));
+  }, [percent]);
 
   useEffect(() => {
     const newField = makeField(fieldSize, fieldSize, active, cells);
     dispatch(actions.setField(newField));
   }, [active]);
+
+  useEffect(() => {
+    let interval: any = null;
+    if (speed > 0) {
+      interval = setInterval(() => {
+        const generation = getGeneration(cells);
+        dispatch(actions.setField(generation));
+      }, 1000 * speed);
+    }
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [speed, cells]);
 
   return (
     <FieldWrapper>
